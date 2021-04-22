119b0f8cf34ed0d97ca318c75f1795cc4e600d82
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-04-21 22:24:52 -0500

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,41 @@
+﻿using Amazon.Lambda.Core;
+
+namespace LambdaUnitTestExample.Tests
+{
+    public enum Condition {EXCELLENT, GOOD, FAIR, BAD};
+
+    public class Car
+    {
+        public Condition carCondition;
+        public string Make;
+        public int speed;
+
+        public Car(string carMake, Condition condition)
+        {
+            this.Make = carMake;
+            this.carCondition = condition;
+        }
+
+        public string make(string Make)
+        {
+            return Make;
+        }
+
+        public static string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (-50 <= value && value <= 200)
+                {
+                    speed = value;
+                }
+            }
+        }
+    }
+}
